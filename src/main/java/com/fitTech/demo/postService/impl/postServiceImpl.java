cab5262e5fbf5c0f582ee1a662b91502b3456c45
Jasiri <jasiridickson@gmail.com>
2024-03-27 19:01:53 -0500

    Creating variables to insert into exchange method"

--- src/main/java/com/fitTech/demo/postService/impl/postServiceImpl.java
+++ src/main/java/com/fitTech/demo/postService/impl/postServiceImpl.java
@@ -1,2 +1,27 @@
-package com.fitTech.demo.postService.impl;public class postServiceImpl {
+package com.fitTech.demo.postService.impl;
+
+import com.fitTech.demo.postService.postService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class postServiceImpl implements postService {
+
+    //Base URL for making requests to edamam API
+    String baseURL = "https://api.edamam.com/api/recipes/v2";
+
+    //StringBuilder to contain URL that we will pass into the exchange method
+    StringBuilder str = new StringBuilder();
+    @Autowired
+    private RestTemplate restTemplate;
+
+    //Importing method from postService
+    @Override
+    public List<Map<String, Object>> getPosts() {
+        return restTemplate.exchange();
+    }
 }
--- src/main/java/com/fitTech/demo/postService/postService.java
+++ src/main/java/com/fitTech/demo/postService/postService.java
@@ -1,2 +1,12 @@
-package com.fitTech.demo.postService;public interface postService {
+package com.fitTech.demo.postService;
+
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface postService {
+
+    //Hashmap meant to represent the results of a request to the API
+    List<Map<String, Object>> getPosts();
 }
